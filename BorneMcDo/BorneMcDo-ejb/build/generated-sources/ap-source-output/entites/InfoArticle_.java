cdfe344c32dca6580d90ab4f5819b8f309e9e497
DESKTOP-7KVNPUR\nico <nicolas.phalempin@gmail.com>
2018-09-25 16:58:29 +0200

    ajout style.css, bootstrap css et JQuery

--- BorneMcDo/BorneMcDo-ejb/build/generated-sources/ap-source-output/entites/InfoArticle_.java
+++ BorneMcDo/BorneMcDo-ejb/build/generated-sources/ap-source-output/entites/InfoArticle_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-25T15:29:06")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-09-25T16:51:21")
 @StaticMetamodel(InfoArticle.class)
 public class InfoArticle_ { 
 
