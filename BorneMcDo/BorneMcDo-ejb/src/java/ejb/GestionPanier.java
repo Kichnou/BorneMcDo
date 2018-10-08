88f942bf566efabaac6e569d93fc4e234d86543d
AFPA-PARIS-DI\cdi308 <ignace.kevin@gmail.com>
2018-10-08 16:43:09 +0200

    no message

--- BorneMcDo/BorneMcDo-ejb/src/java/ejb/GestionPanier.java
+++ BorneMcDo/BorneMcDo-ejb/src/java/ejb/GestionPanier.java
@@ -2,6 +2,7 @@ package ejb;
 
 import entites.Article;
 import entites.Choix;
+import entites.SupplementArticle;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateful;
@@ -52,7 +53,7 @@ public class GestionPanier implements GestionPanierLocal {
 
         c.setTauxTva(a.getLaTva().getTaux());
         c.setPrix(a.getPrix());
-        c.setUnArticle(a);                
+        c.setUnArticle(a);       
 
         this.getMonPanier().add(c);
     }
@@ -61,10 +62,10 @@ public class GestionPanier implements GestionPanierLocal {
         Long id = Long.valueOf(supplement);
         Query tq = em.createQuery("select s from SupplementArticle s where s.id = :paramId");
         tq.setParameter("paramId", id);
-        List<Article> la = tq.getResultList();
-        Article a = null;
-        for (Article art : la) {
-            a = art;
+        List<SupplementArticle> ls = tq.getResultList();
+        SupplementArticle s = null;
+        for (SupplementArticle sup : ls) {
+            s = sup;
         }
         
         this.getMonPanier().get(this.getMonPanier().size() - 1).setUnSuppArt(null);
