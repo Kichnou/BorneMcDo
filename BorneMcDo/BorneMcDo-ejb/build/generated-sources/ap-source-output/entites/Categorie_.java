efeb3bbd077171d18032a00635d15479a73e2b7f
SystemFlo <41999370+SystemFlo@users.noreply.github.com>
2018-10-17 10:45:27 +0200

    no message

--- BorneMcDo/BorneMcDo-ejb/build/generated-sources/ap-source-output/entites/Categorie_.java
+++ BorneMcDo/BorneMcDo-ejb/build/generated-sources/ap-source-output/entites/Categorie_.java
@@ -6,11 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-16T16:02:20")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-17T10:09:38")
->>>>>>> f1d605b9c7334bf2e35d5238f5d9feeddc8ff9e8
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-17T10:35:02")
 @StaticMetamodel(Categorie.class)
 public class Categorie_ { 
 
